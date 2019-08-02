f3dda164ad70c0800810c582a85c0a39dbe7629b
fredkeemhaus <fredkeemhaus@gmail.com>
2019-08-02 19:06:49 +0900

    Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -9,7 +9,7 @@ import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
-// import Detail from "Routes/Detail";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
@@ -19,7 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" component={TV} />
         <Route path="/search" component={Search} />
-        {/* <Route path="/detail" component={Detail} /> */}
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -7,7 +8,35 @@ export default class extends React.Component {
     tvResults: null,
     searchTerm: "",
     error: null,
-    loading: true
+    loading: false
+  };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({ loading: true });
+    try {
+      const {
+        data: { results: movieResults }
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults }
+      } = await tvApi.search(searchTerm);
+      this.setState({
+        movieResults,
+        tvResults
+      });
+    } catch {
+      this.setState({ error: "Can't find results. " });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
@@ -19,6 +48,7 @@ export default class extends React.Component {
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
