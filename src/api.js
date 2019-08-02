4829e1c6ebd70ba3031b0930a6d7a78be86c5686
fredkeemhaus <fredkeemhaus@gmail.com>
2019-08-02 18:38:00 +0900

    Home Container

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
   }
 });
 
-export const MoviesApi = {
+export const moviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   popular: () => api.get("movie/popular"),
