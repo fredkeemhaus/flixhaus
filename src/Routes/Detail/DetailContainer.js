67b4174b3fc007adf46469b0db54a2f8f792abc4
프렛의 브이로그 <fredkeemhaus@gmail.com>
2019-08-05 02:10:59 +0900

    Detail Container part Two

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,28 +1,49 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
-
+import {moviesApi, tvApi} from "api"
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props)
+    const {location: {pathname}} = props
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")
+    };
+  }
+
 
   async componentDidMount() {
     const {
       match: {
         params: {id}
-      }, history: {push}
+      }, 
+      history: {push},
+      
     } = this.props
-
+    const {isMovie} = this.state
     const parseId = parseInt(id);
     if(isNaN(parseId) ) {
       return push('/')
     }
+    let result = null;
+    try{
+      if(isMovie) {
+        ({data:result} = await moviesApi.movieDetail(parseId))
+      } else {
+        ({data:result} = await tvApi.showDetail(parseId))
+      }
+      console.log(result)
+    }catch {
+      this.setState({error: " Can't find anything. "})
+    }finally {
+      this.setState({loading: false, result})
+    }
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.state)
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
