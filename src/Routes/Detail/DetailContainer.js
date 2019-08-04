04e531683f33a1c7b9406780df4afcba3847e147
프렛의 브이로그 <fredkeemhaus@gmail.com>
2019-08-05 01:47:20 +0900

    Detail Container Part One

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,7 +8,21 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    const {
+      match: {
+        params: {id}
+      }, history: {push}
+    } = this.props
+
+    const parseId = parseInt(id);
+    if(isNaN(parseId) ) {
+      return push('/')
+    }
+  }
+
   render() {
+    console.log(this.props)
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,1 @@
-export default () => "contribute check";
+export default () => "TV";
