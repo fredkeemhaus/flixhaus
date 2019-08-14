f851c8d5260ca7a7772093226048a9897e475051
fredkeemhaus <fredkeemhaus@gmail.com>
2019-08-14 14:56:23 +0900

    PResenter Structure

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,45 +1,18 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
-import {moviesApi, tvApi} from "api"
+import { moviesApi, tvApi } from "api";
 export default class extends React.Component {
   constructor(props) {
-    super(props)
-    const {location: {pathname}} = props
+    super(props);
+    const {
+      location: { pathname }
+    } = props;
     this.state = {
       result: null,
       error: null,
       loading: true,
       isMovie: pathname.includes("/movie/")
     };
-  }
-
-
-  async componentDidMount() {
-    const {
-      match: {
-        params: {id}
-      }, 
-      history: {push},
-      
-    } = this.props
-    const {isMovie} = this.state
-    const parseId = parseInt(id);
-    if(isNaN(parseId) ) {
-      return push('/')
-    }
-    let result = null;
-    try{
-      if(isMovie) {
-        ({data:result} = await moviesApi.movieDetail(parseId))
-      } else {
-        ({data:result} = await tvApi.showDetail(parseId))
-      }
-      console.log(result)
-    }catch {
-      this.setState({error: " Can't find anything. "})
-    }finally {
-      this.setState({loading: false, result})
-    }
   }
 
   async componentDidMount() {
@@ -49,15 +22,28 @@ export default class extends React.Component {
       },
       history: { push }
     } = this.props;
+    const { isMovie } = this.state;
     const parseId = parseInt(id);
     if (isNaN(parseId)) {
       return push("/");
     }
+    let result = null;
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parseId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parseId));
+      }
+    } catch {
+      this.setState({ error: " Can't find anything. " });
+    } finally {
+      this.setState({ loading: false, result });
+    }
   }
 
   render() {
-    console.log(this.state)
     const { result, error, loading } = this.state;
+    console.log(result);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
